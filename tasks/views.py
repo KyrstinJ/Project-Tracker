ee6134b13b92682d8da2136464ea93c58e906d25
kyrstin jones <kyrstin.jonesgmail.com>
2023-04-25 23:26:10 -0400

    Feature 15 Complete

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from tasks.models import Task
+from django.contrib.auth.decorators import login_required
+from tasks.forms import TaskForm
 
-# Create your views here.
+
+@login_required
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            project = form.save()
+            project.author = request.user
+            project.save()
+            return redirect("list_projects")
+    else:
+        form = TaskForm()
+    context = {
+        "form": form
+    }
+    return render(request, "projects/create.html", context)
